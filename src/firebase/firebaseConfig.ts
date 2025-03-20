f450a5d1596458be41c8bd423d64730ab7cc0e49
Christian Arévalo Jesús <carevalojesus@gmail.com>
2025-03-20 01:02:56 -0500

    Update firebaseConfig.ts

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -2,15 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-// La configuración de Firebase se carga desde variables de entorno.
-// Es recomendable usar el prefijo "PUBLIC_" para las variables que se necesiten en el cliente.
 const firebaseConfig = {
-  apiKey: import.meta.env.PUBLIC_FIREBASE_API_KEY,
-  authDomain: import.meta.env.PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
 };
 
 // Inicializa la app de Firebase (Firebase se encargará de mantener una única instancia)
